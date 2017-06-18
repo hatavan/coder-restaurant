a2a9f0e3b24fb99957964285680b9eb2c3c604cf
hata <hata@tinypulse.com>
2017-06-19 01:44:49 +0700

    User should be able to search for a menu item and see results.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,16 @@ class HomeController < ApplicationController
     @sections = Section.all
     if params[:section].present?
       @foodItems = FoodItem.where(section_id: params[:section].to_i)
+    elsif params[:search].present?
+      @foodItems = FoodItem.search(params[:search])
     else
       @foodItems = FoodItem.all
     end
-    if params[:sort_column].present?
-      @foodItems = @foodItems.order("#{params[:sort_column]} #{params[:sort_direction]}")
-    end
+    @foodItems = @foodItems.order_or_default(params[:sort_column], params[:sort_direction])
   end
 
   def contact_us
   end
+
+  private 
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -7,4 +7,12 @@ class FoodItem < ApplicationRecord
      "http://loremflickr.com/320/240/#{name.gsub(' ', '+')}"
     end
   end
+
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+  end
+
+  def self.order_or_default(column, direction)
+    order(column.present? ? "#{column} #{direction}" : "name asc")
+  end
 end
